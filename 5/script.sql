5599606b39316ad7e35c422da0239f04376c3687
y4m4t0 <argun63901@gmail.com>
2022-11-17 15:48:07 +0600

    lab 5

--- 5/script.sql
+++ 5/script.sql
@@ -1,4 +1,6 @@
+
 create database lab4;
+
 
 drop table Boxes;
 drop table WAREHOUSES;
@@ -25,6 +27,33 @@ create table Boxes
 );
 
 
+
+
+
+
+
+
+INSERT INTO Warehouses(code, location, capacity) VALUES(1, 'Chicago', 3);
+INSERT INTO Warehouses(code, location, capacity) VALUES(2, 'Rocks', 4);
+INSERT INTO Warehouses(code, location, capacity) VALUES(3, 'New York', 7);
+INSERT INTO Warehouses(code, location, capacity) VALUES(4, 'Los Angeles', 2);
+INSERT INTO Warehouses(code, location, capacity) VALUES(5, 'San Francisko', 8);
+
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('4H8P', 'Rocks', 250, 1);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('0MN7', 'Rocks', 180, 3);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('4RT3', 'Scissors', 190, 4);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('7G3H', 'Rocks', 200, 1);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('8JN6', 'Papers', 75, 1);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('8Y6U', 'Papers', 50, 3);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('9J6F', 'Paapers', 175, 2);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('LL08', 'Rocks', 140, 4);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('P0H6', 'Scissors', 125, 1);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('P2T6', 'Scissors', 150, 2);
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('TUSS', 'Papers', 90, 5);
+
+
+
+
 select * from warehouses ;
 
 select * from boxes where value >= 150;
@@ -36,3 +65,21 @@ select count(id), warehouses from boxes group by warehouses;
 select count(id), warehouses from boxes  group by warehouses having count(id)>2 ;
 
 INSERT INTO Warehouses(code, location, capacity) VALUES(6, 'New York', 3);
+
+
+INSERT INTO Boxes(code, contents, value, warehouses) VALUES ('H5RT', 'Papers', 200, 2);
+
+update Boxes
+set value = value*0.75
+where id = (
+SELECT *
+FROM   (SELECT *, DENSE_RANK() OVER (ORDER BY value DESC) AS rk 
+        FROM   boxes) t
+WHERE  rk = 3;select) 
+;
+
+delete from boxes where value <150;
+
+delete from boxes where warehouses in (select id from warehouses where location='New York') returning *;
+
+
